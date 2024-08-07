a54c6dff77676f83723192712530c37162c0aaf2
theGuardianCode <39659497+theGuardianCode@users.noreply.github.com>
2024-08-07 12:15:41 +1000

    Update main.go

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ func handle_error(err error) {
 }
 
 func main() {
-	// arguments := os.Args
-	arguments := []string{"", "decode", "outfile.png", "document.docx"}
+	arguments := os.Args
+	//arguments := []string{"", "decode", "outfile.png", "document.docx"}
 	path := arguments[2]
 
 	data, err := os.ReadFile(path)
